658e1a9179501cddd77db03043fd831aab5cbef6
Navin balekomebole <navin.balekomebole@stu.ocsb.ca>
2023-02-09 19:32:20 +0000

    added MathOperators.java

---
+++ MathOperators.java
@@ -0,0 +1,33 @@
+/**
+* Math.
+*
+* @author  Navin Baleko
+* @version 1.0
+* @since   2021-11-16
+*/
+
+class MathOperators {
+    /**
+    * Main function.
+    *
+    * @param args Unused
+    */
+    public static void main(String[] args) {
+        // Addition
+        System.out.println("6+6 = " + (6 + 6)); 
+        // subtraction  
+        System.out.println("6-2 = " + (6 - 2)); 
+        // Multiplication
+        System.out.println("12 * 12 = " + (12 * 12)); 
+        // Division with integers
+        System.out.println("8/59 = " + (8 / 59)) ;
+        // Division with float numbers 
+        System.out.println("8/59 = " + (8f / 59f));
+        // Division with double numbers 
+        System.out.println("8/59 = " + (8 / 59));
+        // Exponents with power of 3
+        System.out.println("2^3 = " + (Math.pow(2, 3)));
+        // square root 
+        System.out.println("square root of 169 = " + (Math.sqrt(13)));
+    }
+} 
